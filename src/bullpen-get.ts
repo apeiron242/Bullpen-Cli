27a48aee74a2c5b88f74f9f3a4ec592bc62d82c6
apeiron242 <apeiron242@gmail.com>
2021-03-09 03:02:17 +0900

    updated logic

--- src/bullpen-get.ts
+++ src/bullpen-get.ts
@@ -3,14 +3,16 @@ import scrapeBullpen from "./utils/scrapeBullpen";
 import scrapeKeywords from "./utils/scrapeKeywords";
 
 const getTags = async () => {
+  const list = ["Get All"];
   const tags = await scrapeKeywords();
-  return tags;
+  const lists = list.concat(tags);
+  return lists;
 };
 
 const formatPages = () => {
   const nums: string[] = [];
-  for (let i = 1; i < 15; i++) {
-    nums.push(i + "00");
+  for (let i = 250; i <= 5000; i += 250) {
+    nums.push(String(i));
   }
   return nums;
 };
@@ -27,12 +29,12 @@ inquirer
       name: "pages",
       choices: formatPages,
     },
-    {
-      type: "confirm",
-      name: "inOrder",
-      message: "Scrape in time order? (Not precise)",
-    },
+    // {
+    //   type: "confirm",
+    //   name: "inOrder",
+    //   message: "Scrape in time order? (Not precise)",
+    // },
   ])
   .then(async (result) => {
-    scrapeBullpen(result.tag, result.pages, result.inOrder);
+    scrapeBullpen(result.tag, result.pages);
   });
--- src/utils/scrapeBullpen.ts
+++ src/utils/scrapeBullpen.ts
@@ -3,33 +3,43 @@ import Axios from "axios";
 import * as iconv from "iconv-lite";
 import Format from "./Format";
 
-const scrapeBullpen = async (val: string, pages: string, inOrder: boolean) => {
-  for (let i = 1; i < Number(pages); i += 10) {
+const scrapeBullpen = async (val: string, pages: string) => {
+  for (let i = 1; i < Number(pages); i += 30) {
     const url: string = encodeURI(
-      `http://mlbpark.donga.com/mp/b.php?p=${i}&m=search&b=bullpen&query=${val}`
+      `http://mlbpark.donga.com/mp/b.php?p=${i}&m=list&b=bullpen&query=&select=&user=`
     );
-    if (inOrder) {
-      await getPages(url);
-    } else {
-      getPages(url);
-    }
+
+    await getPages(url, val);
+
+    // if (inOrder) {
+    //   await getPages(url, val);
+    // } else {
+    //   getPages(url, val);
+    // }
   }
 };
 
-const getData = (body: string) => {
+const getData = (body: string, val: string) => {
   const $ = cheerio.load(body);
   const box = $("tbody tr").each((i, elem) => {
     const title = $(elem).find(".tit a").text().trim();
     const link = $(elem).find(".tit a").attr("href");
     const date = $(elem).find(".date").text().trim();
+    const tag = $(elem).find(".list_word").text().trim();
     if (title && link && title !== undefined && link !== undefined) {
-      const data = new Format(title, String(link), date);
-      console.log(data.form());
+      if (val === "Get All") {
+        const data = new Format(title, String(link), date);
+        console.log(data.form());
+      }
+      if (val === tag) {
+        const data = new Format(title, String(link), date);
+        console.log(data.form());
+      }
     }
   });
 };
 
-const getPages = async (url: string) => {
+const getPages = async (url: string, val: string) => {
   const res = await Axios({
     url,
     method: "GET",
@@ -37,7 +47,7 @@ const getPages = async (url: string) => {
   });
 
   const decoded = await iconv.decode(res.data, "utf-8");
-  await getData(decoded);
+  await getData(decoded, val);
 };
 
 export default scrapeBullpen;
